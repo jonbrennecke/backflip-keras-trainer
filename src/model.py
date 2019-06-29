7f50a79028f12f0a38a277f594f5832ba207cd68
Jonathan Brennecke <jbrennecke@opengov.com>
2019-06-28 20:49:06 -0700

    added basic keras model

--- src/model.py
+++ src/model.py
@@ -1,14 +1,49 @@
-# from keras import backend as K
+import tensorflow as tf
 
-from keras.applications import vgg16
+# TODO: load these constants from constants file
+COLOR_IMAGE_SHAPE = (2320, 3088, 3)
+DEPTH_IMAGE_SHAPE = (480, 640, 1)
+SEGMENTATION_IMAGE_SHAPE = (1160, 1544, 1)
 
 
-class Model:
-    pass
-    # model = vgg16()
+class Model(object):
+    color_image_input: tf.keras.Input = None
+    depth_image_input: tf.keras.Input = None
+    segmentation_image_output: tf.keras.layers.Dense = None
+    model: tf.keras.models.Model = None
 
-    # color_image_input = Input(shape=(120, 160, 3), name='color_image_input')
-    # depth_image_input = Input(shape=(120, 160, 3), name='depth_image_input')
-    # segmentation_image_output = Dense(shape=(120, 160, 3), name='segmentation_image_output')
-    # Model(input=[])
-    # model.summary()
+    def __init__(self):
+        super(Model, self).__init__()
+
+        # K.variable
+
+        # define inputs
+        self.color_image_input = tf.keras.Input(
+            shape=COLOR_IMAGE_SHAPE, name="color_image_input"
+        )
+        # self.depth_image_input = tf.keras.Input(
+        #     shape=DEPTH_IMAGE_SHAPE, name="depth_image_input"
+        # )
+
+        layer = tf.keras.layers.Convolution2D(8, 3, 3)(self.color_image_input)
+
+        # define output
+        self.segmentation_image_output = tf.keras.layers.Dense(
+            units=32,
+            input_shape=SEGMENTATION_IMAGE_SHAPE,
+            name="segmentation_image_output",
+        )(layer)
+
+        inputs = [self.color_image_input]  # TODO: add depth_image_input
+        output = [self.segmentation_image_output]
+        self.model = tf.keras.models.Model(inputs=inputs, outputs=output)
+
+    def compile(self):
+        self.model.compile(optimizer=None)  # TODO
+
+    def train(self):
+        # self.model.fit
+        pass
+
+    def summary(self):
+        return self.model.summary()
